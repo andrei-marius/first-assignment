e968f7c5499fa924d1623353efbe8c2dadd048ab
Andrei Marius <andreimantoniu@yahoo.com>
2022-02-03 14:35:39 +0100

    initial commit

--- assignment.js
+++ assignment.js
@@ -5,13 +5,21 @@ const transactions = require('./simple-transaction-data').data;
 // Assignment 1:
 const filterFunction = (data) => {
     return data.filter((transaction) => {
-        // TODO
+        if(transaction.credit_card_company === 'VISA' && parseInt(transaction.transaction_date.substring(0, 4)) >= 2015 && parseInt(transaction.price) >= 50){
+            return true
+        }
     });
 }
 
 
 // Assignment 2:
 const aboveAverageFunction = (data) => {
-
+    return data.map(transaction => {
+        if(parseInt(transaction.price) > 100){
+            transaction.isPriceAboveAverage = true
+        }else{
+            transaction.isPriceAboveAverage = false
+        }
+        return transaction
+    })
 }
-
